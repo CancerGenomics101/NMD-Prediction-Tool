b92f7710c57ca20b422adfe7c24f97145ef649f8
CancerGenomics101 <ashley.sunderland@nhs.net>
2026-04-26 14:09:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -173,8 +173,8 @@ with tab_input:
                 # First 100 bp warning
                 if ptc_c_pos <= 100:
                     st.warning(
-                        "⚠️ **WARNING:** This variant occurs within the first 100 bp. "
-                        "Currently this tool does not acknowledge the use of alternative transcripts. "
+                        "⚠️ **WARNING:** This variant generates a PTC within the first 100 bp. "
+                        "Currently, this tool does not acknowledge the use of alternative transcripts. "
                         "Use scientific judgement."
                     )
 
